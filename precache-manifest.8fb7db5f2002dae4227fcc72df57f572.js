822b67d49f3620eea4a2c31a7e628659983db569
Matt Butler <mattwbutler@gmail.com>
2019-03-19 21:28:06 -0700

    Updates

---
+++ precache-manifest.8fb7db5f2002dae4227fcc72df57f572.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7091863b96845020473",
+    "url": "/mattapp/static/js/main.cebb45f5.chunk.js"
+  },
+  {
+    "revision": "6c5f62653d4870e4e6fb",
+    "url": "/mattapp/static/js/runtime~main.ee5fcbc3.js"
+  },
+  {
+    "revision": "3248732edbb1ad430e2b",
+    "url": "/mattapp/static/js/2.c8623930.chunk.js"
+  },
+  {
+    "revision": "a102394eb3e4d2482ab295142442e23e",
+    "url": "/mattapp/index.html"
+  }
+];
