85d25c9d19ac539998288f207cbea7c22909f964
Matt Butler <mattwbutler@gmail.com>
2019-03-25 21:17:08 -0700

    Refactor Root component out of index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import configureStore from './configureStore';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Root from './components/Root';
+
+const store = configureStore();
 
 render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
 
